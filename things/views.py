be352803f2d36833b624c926394268aadbdd0a16
Munye Jeilani <munye.jeilani@kcl.ac.uk>
2023-10-29 11:41:26 +0000

    Attempt 2

--- things/views.py
+++ things/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .forms import ThingForm
 
-
 def home(request):
-    form = ThingForm()
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            # Process the form data if it's valid
+            # For example, you can save it to the database
+            form.save()
+    else:
+        form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
